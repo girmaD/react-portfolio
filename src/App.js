673c9c0445f0164720819119fe04e2fa27f7f598
Girma Ebssa <girma.ebssa@gmail.com>
2021-02-04 14:40:42 -0800

    includes Router and Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Portfolio from './components/pages/Portfolio';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (    
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" ><About/></Route>         
+        <Route exact path="/contact" component={Contact} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 

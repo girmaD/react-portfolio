afc7f9d366591687545e15a67c920a3d394ca9c2
Girma Ebssa <girma.ebssa@gmail.com>
2021-02-04 18:23:21 -0800

    adds style to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './pages/style.css'
 
 function Footer() {
     return (
-        <footer className="bg-dark text-muted py-3 text-center footer">  
+        <footer className="footer bg-dark text-muted py-3 text-center footer">  
         <div className="container text-center">          
-         <a className="text-white"  href="https://girmad.github.io/">&#169;Girma-2021</a> 
+         <a className="text-white"  href="https://www.linkedin.com/in/girma-ebssa-23aa801b5/">&#169;Girma-2021</a> 
          <div className="icons float-right pr-5">              
               <a className="text-white mr-2" href="https://www.linkedin.com/in/girma-ebssa-23aa801b5/" target="_blank">
                   <i className="fab fa-linkedin"></i>

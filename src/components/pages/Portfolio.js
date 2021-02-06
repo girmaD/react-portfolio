cb58807aba6bb4c6be2b450b922863bf67e006d3
Girma Ebssa <girma.ebssa@gmail.com>
2021-02-05 16:12:38 -0800

    redo portfolio pages using bootstrap card

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,77 +1,37 @@
-import React, { useState} from 'react'
-import './style.css'
+import React, { useState, useEffect} from 'react';
+import './style.css';
+import portfolio from '../../portfolio.json';
 
-function Portfolio() {
-    let initialState = [
-        {
-          src: 'images/portfolioImages/whaleWatching.png',
-          href: 'https://fathomless-springs-89027.herokuapp.com/sightings'
-        },
-        {
-          src: 'images/portfolioImages/tripPlanner.png',
-          href: 'https://girmad.github.io/Trip-Planner/'
-        }
-      ]
-      const [dataArr, setDataArr] = useState(initialState)
+function Portfolio() {   
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        setData(portfolio)
+    }, [data])
+
     return (
-        <main className="portfolio-container container bg-white">            
-            <div className="row ">
-                <div className="col">
-                    <h1 className="text-info about font-weight-bold  pt-4">
-                        Portfolio
-                    </h1>     
-                    <hr/>              
-                </div>               
+        <div className="container pt-3 mt-3 pb-5 mb-5">
+            <p className="h1">Some of my projects</p>            
+            <div className="row pb-3 mb-3">                                     
+                {data.map(item =>                                
+                    <div className="col-lg-6 my-5"> 
+                        <div key={item.id}  className="card" style={{width: "75%", height: "100%" }}>
+                            <img src={item.src} style={{width: "100%", height: "50%" }} className="card-img-top" alt={item.title}/>
+                            <div className="card-body">
+                                <h5 className="card-title">{item.title}</h5>
+                                <p className="card-text">{item.discription}</p>
+                            </div>                            
+                            <div className="card-body">                                
+                                <button href={item.repo} className="btn btn-success my-1 mr-5">See Repo</button>
+                                <button href={item.href} className="btn btn-success">See Live!!</button>
+                            </div>
+                        </div>
+                    </div>
+               
+                )}            
             </div>
-             {dataArr.map(item=> {                  
-                 <div className="row ">                
-                    <div className="col-lg-6">                    
-                        <figure>
-                            <a href={item.href} className="stretched-link" target="_blank"></a>
-                            <img src={item.src} alt="" className="img-fluid rounded portfolio-image"/>                        
-                        </figure>
-                    </div>                                                              
-                </div>
-             }) }               
-            {/* <div className="row ">                
-                <div className="col-lg-6">                    
-                    <figure>
-                        <a href="https://girmad.github.io/Trip-Planner/" className="stretched-link" target="_blank"></a>
-                        <img src="./assets/images/black-history.jpg" alt="" className="img-fluid rounded portfolio-image"/>                        
-                    </figure>
-                </div>               
-                <div className="col-lg-6">
-                    <figure>
-                        <a href="https://girmad.github.io/Responsive_Portfolio/index.html" className="stretched-link"  target="_blank"> </a>
-                        <img src="./assets/images/girma-paris.jpg" alt="" className="img-fluid rounded portfolio-image"/>                       
-                    </figure>
-                </div>                                               
-            </div>            
-            <div className="row my-3">                               
-                <div className="col-lg-6">
-                    <figure>
-                        <a href="https://girmad.github.io/Weather-Dashboard/" className="stretched-link" target="_blank"> </a>
-                        <img src="./assets/images/girma-stockholm.jpg" alt="" className="img-fluid rounded portfolio-image"/>                       
-                    </figure>
-                </div>               
-                <div className="col-lg-6">
-                    <figure>
-                        <a href="https://girmad.github.io/Code_Quiz/" className="stretched-link" target="_blank"></a>
-                        <img src="./assets/images/younger-me.jpg" alt="" className="img-fluid rounded portfolio-image"/>                        
-                    </figure>
-                </div>                                               
-            </div>
-            
-            <div className="row pb-5">
-                <div className="col-lg-6">
-                    <figure>
-                        <a href="https://girmad.github.io/Work-day-scheduler/" className="stretched-link" target="_blank"></a>
-                            <img src="./assets/images/girma-copenhagen.jpg" alt="" className="img-fluid rounded portfolio-image"/>                       
-                    </figure>
-                </div>                                                   
-            </div>                     */}
-        </main>
-    )
+        </div>
+    )  
 }
 
 export default Portfolio;
